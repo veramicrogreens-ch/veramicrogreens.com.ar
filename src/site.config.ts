178dc6e35e7b8ddd0f33b30a409636c6ac6fc5e8
veramicrogreens-ch <veramicrogreens@gmail.com>
2026-09-20 13:47:10 -0300

    Add src/site.config.ts

---
+++ src/site.config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: 'Vera Microgreens',
+  url: 'https://veramicrogreens.com.ar',
+  description:
+    'Microgreens decorativos para restaurantes. Frescura, detalle y presentación para platos que buscan algo más.',
+  email: '',
+  phone: '+54 9 11 6884-8716',
+  whatsapp: 'https://wa.me/5491168848716',
+  instagram: '',
+  facebook: '',
+};
